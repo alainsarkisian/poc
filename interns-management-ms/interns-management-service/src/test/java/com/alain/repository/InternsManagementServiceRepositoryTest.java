d70ccd487fbdab7cfc6fdaf317d3d37be7419037
Alain SARKISIAN <alain.sarkisian@its4u-group.com>
2022-04-15 16:47:13 +0200

    Ajout de flyway pour migrer et faire les tests, branche suivante va utiliser hibernate pour migrer car on vq tester avec @DataJpaTest

--- interns-management-ms/interns-management-service/src/test/java/com/alain/repository/InternsManagementServiceRepositoryTest.java
+++ interns-management-ms/interns-management-service/src/test/java/com/alain/repository/InternsManagementServiceRepositoryTest.java
@@ -3,6 +3,7 @@ package com.alain.repository;
 import com.alain.model.Intern;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
